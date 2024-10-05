07e8c46239e9dce432607a952f70882a3e0cadfc
lucas-lcs <la.devtech@gmail.com>
2024-10-04 22:58:51 -0300

    add example strings

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/4_string/scripts.js
@@ -0,0 +1,5 @@
+console.log(typeof "Oi, meu nome é Lucas");
+console.log(typeof "Eu queria comprar um jatinho.");
+console.log(typeof `Teste`);
+console.log(typeof "Infinity");
+console.log(typeof Infinity);
---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/5_string_avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \n Segunda linha");
+console.log("O meu nome é 'Lucas'");
+console.log('O nome dela é "Henrique"');
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("O " + "meu" + " nome " + "é" + " Lucas.");
